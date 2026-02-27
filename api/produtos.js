be9321ed91934058e33dedc428d4cb3720471c7a
Almeidazin <152418867+Almeidazin@users.noreply.github.com>
2026-02-27 15:12:31 -0400

    Update produtos.js

--- api/produtos.js
+++ api/produtos.js
@@ -1,14 +1,3 @@
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    const produtos = [
-        {
-            id: '1',
-            category: 'Passeio',
-            title: '175/70 R14 • Linha Econômica',
-            price: 279.90
-        }
-    ];
-    
-    res.status(200).json(produtos);
+  res.status(200).json({ mensagem: "API funcionando!" });
 }
